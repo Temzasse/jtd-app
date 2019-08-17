7896eeb58582981db622dc09c254d3279b0203fd
Temzasse <teemu.taskula@taitounited.fi>
2019-08-17 15:04:48 +0300

    fix: footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import logoImg from '../images/logo_black.png';
+import logoImg from '../images/logo_white_black_bg.png';
+import { media } from '../utils';
 import Layout from './Layout';
 import Text from './Text';
 import Gutter from './Gutter';
@@ -21,28 +22,39 @@ const Footer = () => (
         Ota yhteyttä
       </Heading>
 
-      <Gutter dir="vertical" amount={32} />
+      <Gutter dir="vertical" amount={60} />
 
-      <Layout dir={{ sm: 'col' }}>
+      <Layout dir={{ sm: 'col' }} align={{ sm: 'center' }}>
         <Layout.Box>
-          <Layout dir="col">
-            <Text color="#fff">Johanna Syvälahti-Taskula</Text>
-            <Text color="#fff">Sisustussuunnittelija</Text>
-            <Text color="#fff">044-2370181</Text>
-            <Text color="#fff">info@johannataskula.fi</Text>
+          <Layout dir="col" align="center">
+            <Text weight={500} size={18} color="#fff">
+              Johanna Syvälahti-Taskula
+            </Text>
+            <Text weight={500} size={18} color="#fff">
+              Sisustussuunnittelija
+            </Text>
+            <Text weight={500} size={18} color="#fff">
+              044-2370181
+            </Text>
+            <Text weight={500} size={18} color="#fff">
+              <EmailLink
+                href="mailto:info@johannataskula.fi"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                info@johannataskula.fi
+              </EmailLink>
+            </Text>
           </Layout>
         </Layout.Box>
 
         <Gutter sm={{ dir: 'vertical', amount: 32 }} />
 
         <Layout.Box>
-          <Layout dir="col">
-            <Heading as="h2" color="#fff" align="center">
-              Interior Design
-            </Heading>
-            <Heading as="h3" color="#fff" align="center">
-              Johanna Taskula
-            </Heading>
+          <Layout dir="col" align="center" justify="center">
+            <BrandHeading>Interior Design</BrandHeading>
+            <BrandHeading>Johanna Taskula</BrandHeading>
+            <Gutter dir="vertical" />
             <Logo src={logoImg} />
           </Layout>
         </Layout.Box>
@@ -54,9 +66,17 @@ const Footer = () => (
 const Wrapper = styled.div`
   position: relative;
   width: 100%;
-  background-color: ${props => props.theme.black};
-  padding: 300px 0px 200px 0px;
+  background-color: ${props => props.theme.primary[600]};
+  padding: 250px 0px 150px 0px;
   margin-top: 10px;
+
+  ${media.sm`
+    padding: 120px 0px 40px 0px;
+  `}
+
+  ${media.lg`
+    padding: 300px 0px 150px 0px;
+  `}
 `;
 
 const FooterContent = styled.div`
@@ -66,7 +86,7 @@ const FooterContent = styled.div`
 
 const Logo = styled.img`
   height: auto;
-  width: 200px;
+  width: 120px;
 `;
 
 const Svg = styled.svg`
@@ -74,6 +94,18 @@ const Svg = styled.svg`
   top: -10px;
   left: 0;
   right: 0;
+`;
+
+const BrandHeading = styled.h3`
+  font-size: 24px;
+  font-family: 'Cinzel', serif;
+  margin: 0;
+  font-weight: 400;
+  color: #fff;
+`;
+
+const EmailLink = styled.a`
+  color: #fff;
 `;
 
 export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,6 +9,8 @@ const Box = styled.div`
 const Layout = styled.div`
   display: flex;
   width: 100%;
+  ${responsivify('align', 'align-items')};
+  ${responsivify('justify', 'justify-content')};
   ${responsivify('dir', 'flex-direction', { col: 'column', row: 'row' })}
 `;
 
